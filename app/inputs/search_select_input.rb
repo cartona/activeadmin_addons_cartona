5df942c659d1949977ea15b4653275321024ff84
AreejNour <areej.nour@cartona.com>
2024-04-04 16:09:39 +0200

    add default_select_options edits

--- app/inputs/search_select_input.rb
+++ app/inputs/search_select_input.rb
@@ -1,11 +1,10 @@
-class SearchSelectInput < ActiveAdminAddons::SelectInputBase
+class SearchSelectInput < ActiveAdminAddons::InputBase
   include ActiveAdminAddons::SelectHelpers
 
   def render_custom_input
     concat(label_html)
-    concat(
-      builder.select(input_method, initial_collection_to_select_options, {}, input_html_options)
-    )
+    concat(builder.select(input_method,
+                          initial_collection_to_select_options, {}, input_html_options))
   end
 
   def input_method
@@ -13,18 +12,27 @@ class SearchSelectInput < ActiveAdminAddons::SelectInputBase
   end
 
   def load_control_attributes
+    opts = ActiveadminAddons.default_select_options
     load_class(@options[:class])
-    load_data_attr(:fields, default: ["name"], formatter: :to_json)
-    load_data_attr(:predicate, default: "cont")
+    load_data_attr(:fields, default: opts[:fields], formatter: :to_json)
+    load_data_attr(:predicate, default: opts[:predicate])
     load_data_attr(:url, default: url_from_method)
     load_data_attr(:response_root, default: tableize_method)
-    load_data_attr(:display_name, default: "name")
-    load_data_attr(:minimum_input_length, default: 1)
-    load_data_attr(:width)
+    load_data_attr(:display_name, default: opts[:display_name])
+    load_data_attr(:minimum_input_length, default: opts[:minimum_input_length])
+    load_data_attr(:width, default: opts[:width])
+    load_data_attr(:multiple, default: opts[:multiple])
+    load_data_attr(:per_page, default: opts[:per_page])
+    load_data_attr(:query_extras, default: opts[:query_extras])
     load_data_attr(
       :order,
       value: @options[:order_by],
-      default: "#{get_data_attr_value(:fields).first}_desc"
+      default: get_data_attr_value(:fields).first.to_s + "_desc"
     )
+
+    if get_data_attr_value(:multiple)
+      load_attr(:name, value: method_to_input_array_name)
+      load_attr(:multiple, value: "multiple")
+    end
   end
 end
